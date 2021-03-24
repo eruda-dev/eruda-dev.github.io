7c13487755bd79404a42e28ff037aab0df36bcb1
eruda.dev <eruda.dev@gmail.com>
2021-03-24 11:42:27 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    devServer: {
+        contentBase: path.join(__dirname),
+        compress: true,
+        port: 9000,
+    },
+};
